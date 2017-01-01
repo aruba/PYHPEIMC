7c73ba12cc643df5b29623283c17946a580aa483
netmanchris <chris_young@me.com>
2017-01-01 17:08:15 -0500

    modifing variable names for pep8 compliance - pyhpeimc.plat.alarms.py

--- pyhpeimc/plat/alarms.py
+++ pyhpeimc/plat/alarms.py
@@ -53,17 +53,16 @@ def get_dev_alarms(auth, url, devid=None, devip=None):
     get_dev_alarm_url = "/imcrs/fault/alarm?operatorName=admin&deviceId=" + \
                         str(devid) + "&desc=false"
     f_url = url + get_dev_alarm_url
-    # creates the URL using the payload variable as the contents
-    r = requests.get(f_url, auth=auth, headers=HEADERS)
+    response = requests.get(f_url, auth=auth, headers=HEADERS)
     try:
-        if r.status_code == 200:
-            dev_alarm = (json.loads(r.text))
+        if response.status_code == 200:
+            dev_alarm = (json.loads(response.text))
             if 'alarm' in dev_alarm:
                 return dev_alarm['alarm']
             else:
                 return "Device has no alarms"
-    except requests.exceptions.RequestException as e:
-            return "Error:\n" + str(e) + ' get_dev_alarms: An Error has occured'
+    except requests.exceptions.RequestException as error:
+            return "Error:\n" + str(error) + ' get_dev_alarms: An Error has occured'
 
 
 def get_realtime_alarm(username, auth, url):
@@ -96,12 +95,12 @@ def get_realtime_alarm(username, auth, url):
     """
     get_realtime_alarm_url = "/imcrs/fault/faultRealTime?operatorName=" + username
     f_url = url + get_realtime_alarm_url
-    r = requests.get(f_url, auth=auth, headers=HEADERS)
+    response = requests.get(f_url, auth=auth, headers=HEADERS)
     try:
-        realtime_alarm_list = (json.loads(r.text))
+        realtime_alarm_list = (json.loads(response.text))
         return realtime_alarm_list['faultRealTime']['faultRealTimeList']
-    except requests.exceptions.RequestException as e:
-        return "Error:\n" + str(e) + ' get_realtime_alarm: An Error has occured'
+    except requests.exceptions.RequestException as error:
+        return "Error:\n" + str(error) + ' get_realtime_alarm: An Error has occured'
 
 
 def get_alarms(username, auth, url):
@@ -135,14 +134,10 @@ def get_alarms(username, auth, url):
     get_alarms_url = "/imcrs/fault/alarm?operatorName=" + username + \
                      "&recStatus=0&ackStatus=0&timeRange=0&size=50&desc=true"
     f_url = url + get_alarms_url
-    r = requests.get(f_url, auth=auth, headers=HEADERS)
-    # r.status_code
+    response = requests.get(f_url, auth=auth, headers=HEADERS)
     try:
-        if r.status_code == 200:
-            alarm_list = (json.loads(r.text))
+        if response.status_code == 200:
+            alarm_list = (json.loads(response.text))
             return alarm_list['alarm']
-    except requests.exceptions.RequestException as e:
-            return "Error:\n" + str(e) + ' get_alarms: An Error has occured'
-
-
-
+    except requests.exceptions.RequestException as error:
+            return "Error:\n" + str(error) + ' get_alarms: An Error has occured'
